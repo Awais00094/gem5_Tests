fee1048995ea58806c10ba1bb68299b9aa918939
Muhammad Awis <muhammad.awais@telecom-paris.fr>
2025-11-27 15:38:45 +0100

    add the test

---
+++ Test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+
+int main() {
+    
+    printf("RISC-V Test Program: Prefetching and Timing Measurements\n"  );
+    return 0;
+}
